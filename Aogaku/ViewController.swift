4cf6d1c2852ac211574e653cf6352d30dd959f3d
Forta.2k25 <forta.2k25@gmail.com>
2025-08-04 17:06:52 +0900

    Initial Commit

---
+++ Aogaku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aogaku
+//
+//  Created by 米沢怜生 on 2025/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AogakuTests/AogakuTests.swift
@@ -0,0 +1,17 @@
+//
+//  AogakuTests.swift
+//  AogakuTests
+//
+//  Created by 米沢怜生 on 2025/08/04.
+//
+
+import Testing
+@testable import Aogaku
+
+struct AogakuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
